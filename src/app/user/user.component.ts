3005896dfcca909cc50f0eb2031f057dfef8851c
Wigibles <luigisantiago0723@gmail.com>
2025-02-12 20:04:39 +0800

    initial commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+
+const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  displayUser = DUMMY_USERS[randomUser];
 
+  get imageUrl() {
+    return '../assets/users/' + this.displayUser.avatar;
+  }
+
+  onSelectUser() {
+    const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.displayUser = DUMMY_USERS[randomUser];
+  }
 }
